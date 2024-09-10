e997b783047d9b342385fb77cb910c21bc387e3a
Askar-A-A <askar.a.a.dev@gmail.com>
2024-09-10 23:02:50 +0500

    tests for custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -10,13 +10,26 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     """Manager for users."""
+
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+           raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(user=self._db)
+        user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
@@ -28,3 +41,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
